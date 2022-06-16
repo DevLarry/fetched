adf5dbe70d71b1565e568b77d1811dd0e1009a05
DevLarry <olanrewajusamad070@gmail.com>
2022-06-16 02:56:13 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({}));
+app.use(cors());
+app.post("/", (req, res) => {
+	axios
+		.get(req.body.url)
+		.then(result => {
+			res.json({ data: result.data, url: req.body.url });
+		})
+		.catch(err => {
+			console.log(err.message);
+			res.status(404).send("Failed to load: "+req.body.url);
+		});
+});
+
+app.listen(process.env.PORT || 1234, () => console.log("port = 1234"));
